83624ddda8038adcec7da9f934e24cdfc480a84a
Jim Szalewski <jim.szalewski@gmail.com>
2017-10-12 21:37:48 -0600

    adjusted to fit styling

--- spec/features/repositories_spec.rb
+++ spec/features/repositories_spec.rb
@@ -4,12 +4,8 @@ RSpec.feature 'Logged in user visits their repositories page' do
   scenario 'and sees a list of their repositories' do
     VCR.use_cassette('features/repositories') do
       stub_omniauth
-      visit dashboard_index_path
+      visit repositories_path
 
-      # click_link "Repositories"
-      page.find(".repository").click
-
-      expect(current_path).to eq(repositories_path)
       expect(page).to have_css('.repository', count: 46)
       within(first('.repository')) do
         expect(page).to have_css('.name')
